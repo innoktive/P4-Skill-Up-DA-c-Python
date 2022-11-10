ed480b69a9de306de160f0cb5243f379237526f5
ferrero-manuel <manuel.ferrero.710@gmail.com>
2022-11-10 20:34:32 -0300

    Update dags: add logging configurations to dags of group E.

--- dags/GEUNAbiertaInteramericana_dag_etl.py
+++ dags/GEUNAbiertaInteramericana_dag_etl.py
@@ -1,6 +1,24 @@
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+import logging
+
+
+LOGGER_NAME = 'Logger Univ. Abierta Interamericana'
+LOGGER_MSG = ''
+
+def _log():
+    logging.basicConfig(level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(message)s',
+        datefmt='%Y-%m-%d')
+    logger = logging.getLogger(LOGGER_NAME)
+    logger.info('%s', LOGGER_MSG)
+
+
+def extract():
+    _log()
+
 
 univ_abierta_interamericana_DAG = DAG(
     dag_id='univ_abierta_interamericana',
@@ -16,12 +34,12 @@ univ_abierta_interamericana_DAG = DAG(
 
 with univ_abierta_interamericana_DAG as dag:
     # Use postgres hook to extract date from database.
-    extract = EmptyOperator(task_id='extract_data')
+    task_extract = PythonOperator(task_id='extract_data', callable=extract)
 
     # Use pandas in PythonOperator to transform exracted data.
-    transform = EmptyOperator(task_id='transform_data')
+    task_transform = EmptyOperator(task_id='transform_data')
 
     # Use Amazon S3 Hook to load and store transformed data in the cloud.
-    load = EmptyOperator(task_id='load_data')
+    task_load = EmptyOperator(task_id='load_data')
 
-    extract >> transform >> load
+    task_extract >> task_transform >> task_load
--- dags/GEUNNacionalPampa_dag.py
+++ dags/GEUNNacionalPampa_dag.py
@@ -1,6 +1,24 @@
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
 from datetime import datetime, timedelta
+import logging
+
+
+LOGGER_NAME = 'Logger Univ. Nacional de la Pampa'
+LOGGER_MSG = ''
+
+def _log():
+    logging.basicConfig(level=logging.INFO,
+        format='%(asctime)s - %(name)s - %(message)s',
+        datefmt='%Y-%m-%d')
+    logger = logging.getLogger(LOGGER_NAME)
+    logger.info('%s', LOGGER_MSG)
+
+
+def extract():
+    _log()
+
 
 univ_nacional_pampa_DAG = DAG(
     dag_id='univ_nacional_pampa',
@@ -13,15 +31,14 @@ univ_nacional_pampa_DAG = DAG(
     tags=['ETL']
 )
 
-
 with univ_nacional_pampa_DAG as dag:
     # Use postgres hook to extract date from database.
-    extract = EmptyOperator(task_id='extract_data')
+    task_extract = PythonOperator(task_id='extract_data', callable=extract)
 
     # Use pandas in PythonOperator to transform exracted data.
-    transform = EmptyOperator(task_id='transform_data')
+    task_transform = EmptyOperator(task_id='transform_data')
 
     # Use Amazon S3 Hook to load and store transformed data in the cloud.
-    load = EmptyOperator(task_id='load_data')
+    task_load = EmptyOperator(task_id='load_data')
 
-    extract >> transform >> load
+    task_extract >> task_transform >> task_load
